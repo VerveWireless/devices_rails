02d03d8ecf123c24d3d9af04d459727fb2d7528e
Jeff Cooper <jeff@cuperman.net>
2015-11-06 15:45:32 -0800

    Create helpers for device tags

--- app/helpers/devices_rails/application_helper.rb
+++ app/helpers/devices_rails/application_helper.rb
@@ -1,4 +1,55 @@
 module DevicesRails
   module ApplicationHelper
+    def device_tag(options={}, &block)
+      # options:
+      #   - name (iphone6, iphone6plus, ...)
+      #   - color (black, silver, gold)
+      #   - orientation (portrait, landscape)
+      
+      classes = ["marvel-device"]
+      classes << (options[:name] || "iphone6")
+      classes << (options[:color] || "silver")
+      if options[:orientation] == "landscape"
+        classes << options[:orientation]
+      end
+
+      content_tag :div, class: classes do
+        [
+          content_tag(:div, nil, class: "top-bar"),
+          content_tag(:div, nil, class: "sleep"),
+          content_tag(:div, nil, class: "volume"),
+          content_tag(:div, nil, class: "camera"),
+          content_tag(:div, nil, class: "sensor"),
+          content_tag(:div, nil, class: "speaker"),
+          content_tag(:div, nil, class: "screen") { yield },
+          content_tag(:div, nil, class: "home"),
+          content_tag(:div, nil, class: "bottom-bar")
+        ].join.html_safe
+      end
+    end
+    
+    def iphone6_tag(options={}, &block)
+      device_tag(options.merge(name: "iphone6")) do
+        yield
+      end
+    end
+    
+    def iphone6plus_tag(options={}, &block)
+      device_tag(options.merge(name: "iphone6plus")) do
+        yield
+      end
+    end
+    
+    def ipad_tag(options={}, &block)
+      device_tag(options.merge(name: "ipad")) do
+        yield
+      end
+    end
+    
+    def ipad_native_tag(options={}, &block)
+      device_tag(options.merge(name: "ipad-native")) do
+        yield
+      end
+    end
   end
 end
